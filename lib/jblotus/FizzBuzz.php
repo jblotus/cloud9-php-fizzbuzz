6fdfa7c15654c1bd5c76591adf2d514dc6f0a02e
OpenShift guest <jblotus@gmail.com>
2012-12-24 16:36:24 -0500

    added working fizzbuzz and tests

--- lib/jblotus/FizzBuzz.php
+++ lib/jblotus/FizzBuzz.php
@@ -1,9 +1,54 @@
 <?php
 namespace jblotus;
 
-class FizzBuzz implements \Countable {
+class FizzBuzz implements \Iterator, \Countable {
     
-    public function count() {
-        return 100;
+    const FIZZ = 'Fizz';
+    const BUZZ = 'Buzz';
+    
+    protected $_position = 1;
+    protected $_end = 100;
+    
+    public function count() {        
+        return $this->_end;
     }
+    
+    public function current() {
+
+        $is_multiple_of_three = $this->_position % 3 === 0;
+        $is_multiple_of_five = $this->_position % 5 === 0;
+        
+        if (!$is_multiple_of_three && !$is_multiple_of_five) {
+            return $this->_position;
+        }  
+        
+        $output = '';      
+        if ($is_multiple_of_three) {
+            
+            $output .= self::FIZZ;    
+        }
+        
+        if ($is_multiple_of_five) {
+            $output .= self::BUZZ;
+        }
+            
+        return $output;        
+    }
+    
+    public function next() {
+        ++$this->_position;        
+    }
+    
+    public function rewind() {        
+        $this->_position = 1;
+    }    
+    
+    public function key() {                
+        return $this->_position;
+    }
+    
+    public function valid() {            
+        return $this->_position <= $this->_end;
+    }
+    
 }
--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -1,23 +1,41 @@
 <?php
 use jblotus\FizzBuzz as FizzBuzz;
 
-class FizzBuzzTest extends PHPUnit_Framework_TestCase {
+class FizzBuzzTest extends PHPUnit_Framework_TestCase {    
     
-    public function setUp() {
+    public function testFizzBuzzIsOneHundredBleepsLong() {
         
-    }
-    
-    public function tearDown() {
-        
-    }
-    
-    /**
-     * Test FizzBuzz Output
-     */
-    public function testFizzBuzzHas100Iterations() {
-        
-        $fizzBuzz = new FizzBuzz;
-        
+        $fizzBuzz = new FizzBuzz;        
         $this->assertCount(100, $fizzBuzz);        
     }
+    
+    public function testFizzBuzzOutput() {
+        
+        $fizzBuzz = new FizzBuzz;        
+        
+        $i = 0;
+        
+        foreach ($fizzBuzz as $bleep) {
+            $i++;
+            echo $bleep . "\n";
+            
+            $is_multiple_of_three = $i % 3 === 0;
+            $is_multiple_of_five = $i % 5 === 0;
+                        
+            if ($is_multiple_of_three && $is_multiple_of_five) {
+                $this->assertEquals('FizzBuzz', $bleep, 'Should output FizzBuzz');
+            } else if ($is_multiple_of_three) {
+                $this->assertEquals('Fizz', $bleep, 'Should output only Fizz');
+            } else if ($is_multiple_of_five) {
+                $this->assertEquals('Buzz', $bleep, 'Should output only Buzz');
+            } else {
+                $this->assertEquals($i, $bleep, 'Should output the number');
+            }            
+            
+        }
+        
+        $this->assertEquals(100, $i, 'We should have gone through 100 iterations');
+    }
+    
+     
 }
